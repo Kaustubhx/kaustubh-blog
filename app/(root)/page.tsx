d0a22a2d79078713704b54d9b55d7dfb8b5bc917
Kaustubhx <prime.ganekar@gmail.com>
2024-01-11 22:14:00 +0530

    New: Coming Soon Alert

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex">
-      
+    <main className="flex items-center justify-center h-[50vh]">
+      <h1 className='text-7xl font-bold'>Coming Soon... Stay tuned</h1>
     </main>
   )
 }
